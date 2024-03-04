57d56ee89c85b6c772e63df43c8285ebfa75b340
kiumad <mirfatemi7@gmail.com>
2024-03-05 01:06:35 +0330

    test

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + "; " + expires + "; path=/";
+    document.cookie += name + "=" + value + "; " + expires + "; path=/";
   };
 
   const rejectCookies = () => {
